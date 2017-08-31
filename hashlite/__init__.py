089bdcb1f6b4fd7ce60c9117e3c88910c1f5902f
samvarankashyap <srallaba@redhat.com>
2017-08-31 14:30:28 -0400

    Added Hashlitedb class

--- hashlite/__init__.py
+++ hashlite/__init__.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
+import re
 import json
+from collections import OrderedDict
 from decorators import wrap
 from utils import savedb, loaddb
+# ---- Logic Start ----------------------#
 
-class hashlite:
+class Hashlite:
     def __init__(self, path, reset=False):
         self.path = path
         if os.path.exists(path):
@@ -15,9 +20,9 @@ class hashlite:
                 dbfile.write(json.dumps(self.db))
 
     @wrap(loaddb, savedb)
-    def set(self, key, value, subkey=None, force=False):
+    def set(self, key=None, value=None, subkey=None, force=False):
         try:
-            if subkey and self.db.get(key, False):
+            if subkey!=None and self.db.get(key, False) !=False:
                 if type(self.db.get(key, None)) is list:
                     self.db[key][subkey] = value
                     return True
@@ -58,7 +63,7 @@ class hashlite:
         
         
     @wrap(loaddb, savedb)
-    def listcreate(self, key, subkey=None, force=False):
+    def listcreate(self, key=None, subkey=None, force=False):
         if subkey and self.db.get(key, None) != None:
             if force:
                 # when you want that key to reset 
@@ -86,7 +91,34 @@ class hashlite:
                     return False
 
     @wrap(loaddb, savedb)
-    def listpop(self, key, subkey=None):
+    def dictcreate(self, key=None, subkey=None, force=False):
+        if subkey and self.db.get(key, None) != None:
+            if force:
+                # when you want that key to reset
+                self.db[key][subkey] = {}
+            else:
+                # when db[key] is a list we dont perform
+                if type(self.db[key]) is list:
+                    print("Operation cant be performed")
+                # when its a dict use setdefault and confirm it happened or not
+                if type(self.db[key]) is dict:
+                    r = self.db[key].setdefault(subkey, {})
+                    if r == {}:
+                        return True
+                    else:
+                        return False
+        else:
+            if force:
+                self.db[key] = {}
+            else:
+                r = self.db.setdefault(key, {})
+                if r == {}:
+                    return True
+                else:
+                    return False
+
+    @wrap(loaddb, savedb)
+    def listpop(self, key=None, subkey=None):
         if subkey != None and self.db.get(key, False): 
             if type(self.db[key]) is list:
                 # operation not permitted
@@ -112,7 +144,7 @@ class hashlite:
         return False
 
     @wrap(loaddb, savedb)
-    def listpush(self, key, value, subkey=None):
+    def listpush(self, key=None, value=None, subkey=None):
         if subkey != None and type(self.db.get(key, False)) is dict:
             if subkey in self.db[key] and \
                type(self.db[key].get(subkey, False)) is list:
@@ -129,7 +161,7 @@ class hashlite:
         return False
 
     @wrap(loaddb, savedb)
-    def delete(self, key, subkey=None):
+    def delete(self, key=None, subkey=None):
         if subkey != None and self.db.get(key, None):
             if type(self.db[key]) is dict:
                 return self.db[key].pop(key, False)
@@ -141,36 +173,21 @@ class hashlite:
             return self.db.pop(key, False)
   
     @wrap(loaddb, savedb)
-    def listreset(self, key, subkey=None):
+    def listreset(self, key=None, subkey=None):
         return self.lcreate(key, subkey, force=True)
+
+    @wrap(loaddb, savedb)
+    def getfirst(self, key=None, subkey=None):
+        out = self.db
+        for dkey in sorted(out.keys(), reverse=True):
+            if re.match(key, dkey):
+                if subkey!= None and subkey in out[dkey]:
+                    return out[dkey][subkey]
+                else:
+                    return out[dkey]
+        return False
 
     @wrap(loaddb, savedb)
     def emptydb(self):
         self.db = {}
         return True
-
-# testscript
-#tdb = hashlite("/tmp/tdb.json")
-#tdb.set("hello",[])
-#tdb.set("hai",{})
-#tdb.set("some",[1,2, "apples", "otherthings"])
-#print tdb.get("some")
-#tdb.emptydb()
-#print tdb.get()
-#print tdb.get("some")
-#tdb.set("testlcreate_dict", {})
-#print tdb.lcreate("testlcreate_dict","lcreate_sub_key")
-#print tdb.listreset("testlcreate_dict","lcreate_sub_key")
-#print("lcreate_list")
-#print tdb.get()
-#print tdb.listpop("testlcreate_dict", "lcreate_sub_key" )
-#print tdb.listpop("tcreate")
-#print tdb.get()
-#print tdb.listpush("hai", "testkeyshouldnotenter")
-#print tdb.listpush("some", "testkeyshouldenter")
-#print tdb.listpop("some")
-#print tdb.get()
-#tdb.listcreate("hai", "newlist")
-#print tdb.get()
-#print tdb.listpush("hai", "someval", "newlist")
-#print tdb.get()
